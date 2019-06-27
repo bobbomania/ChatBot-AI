2ef169d05de1f3a0069f851603788f733b90b4b3
Gabriele Trotta <45770128+bobbomania@users.noreply.github.com>
2019-06-27 14:10:23 +0100

    commit

--- trainingBot.py
+++ trainingBot.py
@@ -9,6 +9,7 @@ def cfd(tokenisedText):
     questionsArr = []
     
     processedAnswers = [[]]
+    answersFrequency = []
     
     lineNb = 0
     index = -1
@@ -17,34 +18,49 @@ def cfd(tokenisedText):
         lineNb += 1
         
         if lineNb%2 == 0:
-            answersArr.append(line)
+            answersArr.append(line.strip('\n'))
         
         else:
-            questionsArr.append(line)
+            questionsArr.append(line.strip('\n'))
         
     frequenciesToken = []
 
     for token in tokenisedText:
         for question in questionsArr:
+            
+            questionParsed = parser()
+            questionParsed.text = question
+
             frequency = 0
             
-            if token in question:
-                
-                questionParsed = parser()
-                questionParsed.text = question
-                frequency = 1/(len(questionParsed.parse())-1)
-                frequenciesToken.append(frequency)
+            if token in questionParsed.parse():
+
+                frequency = 1/(len(questionParsed.parse()))
+                frequenciesToken.append((frequency,questionsArr.index(question)))
     
     for answer in answersArr:
         index += 1
-        if len(frequenciesToken) >= index:
+        if len(frequenciesToken)-1 >= index:
             processedAnswers.append([answer,frequenciesToken[index]])
         
         else:
             processedAnswers.append([answer,''])
     
     if len(frequenciesToken) != 0:
-        bestAnswer = answersArr[frequenciesToken.index(max(frequenciesToken))]
+
+       for answer in answersArr:
+            answerFrequency = 0
+           
+            for frequency in frequenciesToken:
+ 
+                if answersArr.index(answer) == frequency[1]:
+                    answerFrequency += frequency[0]
+
+            answersFrequency.append(answerFrequency)
+    
+    if len(answersFrequency) != 0:
+        bestAnswer = answersArr[answersFrequency.index(max(answersFrequency))]
+       
     
     else:
         bestAnswer = random.choice(answersArr)
